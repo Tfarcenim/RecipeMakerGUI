d0bcc936e9c603e9f52ff181329337210cbae8cd
Tfarcenim <44327798+Tfarcenim@users.noreply.github.com>
2024-10-16 13:57:34 -0400

    Update CraftingRecipeMakerMenu.java

--- common/src/main/java/tfar/recipemakergui/menu/CraftingRecipeMakerMenu.java
+++ common/src/main/java/tfar/recipemakergui/menu/CraftingRecipeMakerMenu.java
@@ -10,6 +10,7 @@ import net.minecraft.world.inventory.ContainerData;
 import net.minecraft.world.inventory.MenuType;
 import net.minecraft.world.inventory.SimpleContainerData;
 import net.minecraft.world.inventory.Slot;
+import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.crafting.Ingredient;
 import net.minecraft.world.item.crafting.RecipeSerializer;
@@ -17,8 +18,7 @@ import org.jetbrains.annotations.Nullable;
 import tfar.recipemakergui.RecipeMakerGUI;
 import tfar.recipemakergui.init.ModMenuTypes;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 import java.util.stream.IntStream;
 
 public class CraftingRecipeMakerMenu extends RecipeMakerMenu {
@@ -43,6 +43,8 @@ public class CraftingRecipeMakerMenu extends RecipeMakerMenu {
         write(jsonObject, name);
     }
 
+    final String pattern = "abcdefghi";
+
     @Override
     protected void serializeRecipeData(JsonObject jsonobject) {
         ItemStack result = craftingInventory.getItem(0);
@@ -54,17 +56,71 @@ public class CraftingRecipeMakerMenu extends RecipeMakerMenu {
                     ingredients.add(Ingredient.of(stack.getItem()));
                 }
             }
-
             JsonArray jsonarray = new JsonArray();
             for(Ingredient ingredient : ingredients) {
                 jsonarray.add(ingredient.toJson());
             }
             jsonobject.add("ingredients", jsonarray);
         } else {
+            writeShaped(jsonobject);
+           // jsonobject.addProperty("show_notification", this.showNotification);
         }
 
         JsonObject resultObj = serializeItemStack(result,saveNBT());
         jsonobject.add("result", resultObj);
+    }
+
+    protected void writeShaped(JsonObject jsonObject) {
+        Map<Item,Character> unique = new HashMap<>();
+        int j = 0;
+        for (int i = 1; i < 10; i++) {
+            ItemStack stack = craftingInventory.getItem(i);
+            if (!stack.isEmpty()) {
+                if (unique.containsKey(stack.getItem())) {
+
+                } else {
+                    unique.put(stack.getItem(),pattern.charAt(j));
+                    j++;
+                }
+
+            }
+        }
+
+        List<String> rows = new ArrayList<>();
+
+        for (int y = 0; y < 3; y++) {
+            StringBuilder row = new StringBuilder();
+            for (int x = 0; x < 3;x++) {
+                ItemStack stack = craftingInventory.getItem(3 * y + x + 1);
+                if (!stack.isEmpty()) {
+                    row.append(unique.get(stack.getItem()));
+                } else {
+                    row.append(" ");
+                }
+            }
+            rows.add(row.toString());
+        }
+
+        Map<Character,Ingredient> keys = new HashMap<>();
+
+        for (Map.Entry<Item,Character> entry : unique.entrySet()) {
+            keys.put(entry.getValue(),Ingredient.of(entry.getKey()));
+        }
+
+        JsonArray jsonarray = new JsonArray();
+
+        for(String s : rows) {
+            jsonarray.add(s);
+        }
+
+        jsonObject.add("pattern", jsonarray);
+        JsonObject keyObject = new JsonObject();
+
+        for(Map.Entry<Character, Ingredient> entry : keys.entrySet()) {
+            keyObject.add(String.valueOf(entry.getKey()), entry.getValue().toJson());
+        }
+
+        jsonObject.add("key", keyObject);
     }
 
     @Override
