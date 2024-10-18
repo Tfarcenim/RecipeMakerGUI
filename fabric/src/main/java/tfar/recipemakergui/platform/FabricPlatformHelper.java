a7209c3f084f4e31c8aaba7ee9986cbfe1f59ce5
Tfarcenim <44327798+Tfarcenim@users.noreply.github.com>
2024-10-18 18:44:05 -0400

    Update FabricPlatformHelper.java

--- fabric/src/main/java/tfar/recipemakergui/platform/FabricPlatformHelper.java
+++ fabric/src/main/java/tfar/recipemakergui/platform/FabricPlatformHelper.java
@@ -1,10 +1,15 @@
 package tfar.recipemakergui.platform;
 
 import net.minecraft.core.Registry;
+import net.minecraft.network.FriendlyByteBuf;
+import net.minecraft.server.level.ServerPlayer;
+import tfar.recipemakergui.network.client.S2CModPacket;
+import tfar.recipemakergui.network.server.C2SModPacket;
 import tfar.recipemakergui.platform.services.IPlatformHelper;
 import net.fabricmc.loader.api.FabricLoader;
 
 import java.util.Map;
+import java.util.function.Function;
 
 public class FabricPlatformHelper implements IPlatformHelper {
 
@@ -27,6 +32,26 @@ public class FabricPlatformHelper implements IPlatformHelper {
 
     @Override
     public <F> void registerAll(Map<String, ? extends F> map, Registry<F> registry, Class<? extends F> filter) {
+
+    }
+
+    @Override
+    public <MSG extends S2CModPacket> void registerClientPacket(Class<MSG> packetLocation, Function<FriendlyByteBuf, MSG> reader) {
+
+    }
+
+    @Override
+    public <MSG extends C2SModPacket> void registerServerPacket(Class<MSG> packetLocation, Function<FriendlyByteBuf, MSG> reader) {
+
+    }
+
+    @Override
+    public void sendToClient(S2CModPacket msg, ServerPlayer player) {
+
+    }
+
+    @Override
+    public void sendToServer(C2SModPacket msg) {
 
     }
 }
